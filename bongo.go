1e6250e876e488f36e1b96eee42db4efc1c3bb6e
ShoYou <460429192@qq.com>
2014-12-31 17:03:16 +0800

    Update bongo.go

--- bongo.go
+++ bongo.go
@@ -17,6 +17,7 @@ import (
 // Define type interface accept different route
 type Handler interface{}
 
+// Validate handler if it's not a func type
 func validateHandler(handler Handler) {
     if reflect.TypeOf(handler).Kind() != reflect.Func {
         panic("bongo handler must be a callable func")
@@ -27,11 +28,10 @@ func validateHandler(handler Handler) {
 type Bongo struct {
     inject.Injector
     *Logger
-    handler *mux.Router
-    config  map[string]interface{}
+    config map[string]interface{}
 }
 
-// New a bongo application
+// New a Bongo application
 func NewBongo() *Bongo {
     b := &Bongo{
         Injector: inject.New(),
@@ -40,7 +40,20 @@ func NewBongo() *Bongo {
     return b
 }
 
-// Application include Bongo instance and router information
+//  Application include Bongo instance and router information
+//  package main
+//
+//  import "github.com/feiquanbifeng/bongo"
+//
+//  func main() {
+//    b := bongo.App()
+//
+//    b.Get("/", func() string {
+//      return "Hello world!"
+//    })
+//
+//    b.Run(3000)
+//  }
 type Application struct {
     *Bongo
     Router
@@ -59,7 +72,7 @@ func App() *Application {
 func (b *Bongo) Run(port int) error {
     var addr string
     if port == 0 {
-        addr = ":8080"
+        addr = ":3000"
     } else {
         addr = ":" + strconv.Itoa(port)
     }
@@ -67,25 +80,20 @@ func (b *Bongo) Run(port int) error {
     go func() {
         chErrors <- b.listen(addr)
     }()
-
-    err := <-chErrors
-    if err != nil {
-        return err
-    }
-    return nil
+    return <-chErrors
 }
 
-func Route(route string, fn func()) error {
-    h := newRouter(route, fn)
-    bon.handler = h.R
-    return nil
+// ServeHTTP is the HTTP Entry point for a Bongo instance.
+// Useful if you want to control your own HTTP server.
+func (b *Bongo) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+
 }
 
 // Start the app
 func (b *Bongo) listen(port string) error {
     server := http.Server{
         Addr:        port,
-        Handler:     b.handler,
+        Handler:     b,
         ReadTimeout: 5 * time.Second,
     }
     b.Info("listen on %s (%s)\n", port, "dev")
@@ -98,7 +106,7 @@ func (b *Bongo) Get(key string) interface{} {
 }
 
 // Set the value for config variable or with pair
-// If have the same value the before value will be over
+// If have the same value the before value will be override
 func (b *Bongo) Set(key string, value interface{}) {
     if b.config == nil {
         b.config = make(map[string]interface{})
